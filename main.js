b201d70f6303a4dc27daa2a13630557bee852c95
Melker Dahl <jaghetermelker@gmail.com>
2024-02-10 18:40:28 +0100

    createandappend

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 import Fighter from "./modules/Fighter.js";
 import { Warrior, Assassin } from "./modules/Characters.js";
+
+function createAndAppendEl(type, content, container) {
+  const element = document.createElement(type);
+  container.append(element);
+  if (type === "img") {
+    element.src = content;
+  } else {
+    element.innerHTMl = content;
+  }
+  return element;
+}
