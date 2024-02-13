0371c5c97bea3fc4be605be0f00b25494d11530f
Melker Dahl <jaghetermelker@gmail.com>
2024-02-13 15:37:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 import Fighter from "./modules/Fighter.js";
 import { Warrior, Assassin } from "./modules/Characters.js";
 import {
-	displayAssassinForP1,
-	displayAssassinForP2,
-	displayWarriorForP1,
-	displayWarriorForP2,
-	nextTurnP1,
-	nextTurnP2,
-	displayP1win,
-	displayP2win,
+  displayAssassinForP1,
+  displayAssassinForP2,
+  displayWarriorForP1,
+  displayWarriorForP2,
+  nextTurnP1,
+  nextTurnP2,
 } from "./modules/display.js";
 
 const mainContainer = document.querySelector("#mainContainer");
@@ -16,31 +14,31 @@ let player1, player2, warrior1, warrior2;
 
 const formEl = document.querySelector("#gameForm");
 formEl.addEventListener("submit", (event) => {
-	event.preventDefault();
-	player1 = document.querySelector("#p1Name").value;
-	player2 = document.querySelector("#p2Name").value;
-	const characterOptPlayer1 = document.getElementById("player1Character").value;
-	const characterOptPlayer2 = document.getElementById("player2Character").value;
-	mainContainer.innerHTML = "";
+  event.preventDefault();
+  player1 = document.querySelector("#p1Name").value;
+  player2 = document.querySelector("#p2Name").value;
+  const characterOptPlayer1 = document.getElementById("player1Character").value;
+  const characterOptPlayer2 = document.getElementById("player2Character").value;
+  mainContainer.innerHTML = "";
 
-	if (characterOptPlayer1 === "Warrior") {
-		displayWarriorForP1(player1);
-		document.getElementById("warriorUlt1").classList.add("hide");
-		console.log(warrior1);
-	} else if (characterOptPlayer1 === "Assassin") {
-		displayAssassinForP1(player1);
+  if (characterOptPlayer1 === "Warrior") {
+    displayWarriorForP1(player1);
+    document.getElementById("warriorUlt1").classList.add("hide");
+    console.log(warrior1);
+  } else if (characterOptPlayer1 === "Assassin") {
+    displayAssassinForP1(player1);
 
-		document.getElementById("assassinUlt1").classList.add("hide");
-	}
+    document.getElementById("assassinUlt1").classList.add("hide");
+  }
 
-	if (characterOptPlayer2 === "Warrior") {
-		displayWarriorForP2(player2);
-		document.getElementById("warriorUlt2").classList.add("hide");
-	} else if (characterOptPlayer2 === "Assassin") {
-		displayAssassinForP2(player2);
-		document.getElementById("assassinUlt2").classList.add("hide");
-	}
-	nextTurnP1();
+  if (characterOptPlayer2 === "Warrior") {
+    displayWarriorForP2(player2);
+    document.getElementById("warriorUlt2").classList.add("hide");
+  } else if (characterOptPlayer2 === "Assassin") {
+    displayAssassinForP2(player2);
+    document.getElementById("assassinUlt2").classList.add("hide");
+  }
+  nextTurnP1();
 });
 // displayP1win();
 // const warrior1HP = document.getElementById("warrior1HP");
