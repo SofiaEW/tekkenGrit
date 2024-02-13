c9d00555ca81a778ac8918db95577988e7f47474
Melker Dahl <jaghetermelker@gmail.com>
2024-02-13 18:09:40 +0100

    cleanup

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-import Fighter from "./modules/Fighter.js";
 import { Warrior, Assassin } from "./modules/Characters.js";
 import {
-  // displayAssassinForP1,
-  // displayAssassinForP2,
-  // displayWarriorForP1,
-  // displayWarriorForP2,
   displayPlayerOne,
   displayPlayerTwo,
   nextTurnP1,
@@ -15,35 +10,6 @@ const mainContainer = document.querySelector("#mainContainer");
 let p1, p2;
 
 const formEl = document.querySelector("#gameForm");
-// formEl.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   player1 = document.querySelector("#p1Name").value;
-//   player2 = document.querySelector("#p2Name").value;
-//   const characterOptPlayer1 = document.getElementById("player1Character").value;
-//   const characterOptPlayer2 = document.getElementById("player2Character").value;
-//   mainContainer.innerHTML = "";
-
-//   if (characterOptPlayer1 === "Warrior") {
-//     displayWarriorForP1(player1);
-//     document.getElementById("warriorUlt1").classList.add("hide");
-//     console.log(warrior1);
-//   } else if (characterOptPlayer1 === "Assassin") {
-//     displayAssassinForP1(player1);
-
-//     document.getElementById("assassinUlt1").classList.add("hide");
-//   }
-
-//   if (characterOptPlayer2 === "Warrior") {
-//     displayWarriorForP2(player2);
-//     document.getElementById("warriorUlt2").classList.add("hide");
-//   } else if (characterOptPlayer2 === "Assassin") {
-//     displayAssassinForP2(player2);
-//     document.getElementById("assassinUlt2").classList.add("hide");
-//   }
-//   nextTurnP1();
-// });
-
-// // inpur från form
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -68,29 +34,11 @@ formEl.addEventListener("submit", (event) => {
     p2 = new Assassin(nameP2);
     displayPlayerTwo(p2);
   }
+
   document.getElementById("player1name").innerText = nameP1;
   document.getElementById("player2name").innerText = nameP2;
   document.getElementById("player1Ult").classList.add("hide");
   document.getElementById("player2Ult").classList.add("hide");
 
-  console.log(nameP1, nameP2);
-
-  console.log(p1, p2);
-  console.log(selectP1, selectP2);
   nextTurnP1();
 });
-
-// // samma för p2
-
-// displayPlayerOne(p1)
-// if(p1.type === 'warrior')
-// p1.daggerAttack(p2)
-// function hideUlt(player) {
-//   if (player === p1) {
-//     const hideUltP1 = document.getElementById("player1Ult");
-//     hideUltP1.classList.add("hide");
-//   } else if (player === p2) {
-//     const hideUltP2 = document.getElementById("player2Ult");
-//     hideUltP2.classList.add("hide");
-//   }
-// }
--- modules/display.js
+++ modules/display.js
@@ -1,6 +1,4 @@
-import { Assassin, Warrior } from "./Characters.js";
 const mainContainer = document.querySelector("#mainContainer");
-let p1, p2, nameP1, nameP2, fighterDiv;
 const selectP1 = document.getElementById("player1Character").value;
 const selectP2 = document.getElementById("player2Character").value;
 
@@ -69,71 +67,6 @@ function displayPlayerTwo(p2) {
   console.log(selectP1, selectP2);
 }
 
-// async function displayWarriorForP1(player1) {
-//   const fighter1Div = document.createElement("div");
-//   fighter1Div.classList.add("flexBox");
-//   fighter1Moves = document.createElement("div");
-//   fighter1Moves.classList.add("flex", "fighter1Moves");
-//   warrior1 = new Warrior();
-//   createAndAppendEl("h1", player1, "playername", fighter1Div);
-//   createAndAppendEl("p", warrior1.health, "warrior1HP", fighter1Div);
-//   createAndAppendEl("img", "./img/", "warriorImg", fighter1Div);
-//   fighter1Div.append(fighter1Moves);
-//   createAndAppendEl("button", "Knuckles", "knuckles", fighter1Moves);
-//   createAndAppendEl("button", "Uppercut", "uppercut", fighter1Moves);
-//   createAndAppendEl("button", "Special", "warriorSpec", fighter1Moves);
-//   createAndAppendEl("button", "Ultimate", "warriorUlt1", fighter1Moves);
-//   mainContainer.append(fighter1Div);
-// }
-// async function displayWarriorForP2(player2) {
-//   const fighter2Div = document.createElement("div");
-//   fighter2Div.classList.add("flexBox");
-//   fighter2Moves = document.createElement("div");
-//   fighter2Moves.classList.add("flex", "fighter2Moves");
-//   warrior2 = new Warrior();
-//   createAndAppendEl("h1", player2, "playername", fighter2Div);
-//   createAndAppendEl("p", warrior2.health, "warrior2HP", fighter2Div);
-//   createAndAppendEl("img", "./img/", "warriorImg", fighter2Div);
-//   fighter2Div.append(fighter2Moves);
-//   createAndAppendEl("button", "Knuckles", "knuckles", fighter2Moves);
-//   createAndAppendEl("button", "Uppercut", "uppercut", fighter2Moves);
-//   createAndAppendEl("button", "Special", "warriorSpec", fighter2Moves);
-//   createAndAppendEl("button", "Ultimate", "warriorUlt2", fighter2Moves);
-//   mainContainer.append(fighter2Div);
-// }
-
-// async function displayAssassinForP1(player1) {
-//   const fighter1Div = document.createElement("div");
-//   fighter1Div.classList.add("flexBox");
-//   fighter1Moves = document.createElement("div");
-//   fighter1Moves.classList.add("flex", "fighter1Moves");
-//   assassin1 = new Assassin();
-//   createAndAppendEl("h1", player1, "playername", fighter1Div);
-//   createAndAppendEl("p", assassin1.health, "assassin1HP", fighter1Div);
-//   createAndAppendEl("img", "./img/", "assasinImg", fighter1Div);
-//   fighter1Div.append(fighter1Moves);
-//   createAndAppendEl("button", "Dagger", "daggerBtn", fighter1Moves);
-//   createAndAppendEl("button", "Legsweep", "legsweepBtn", fighter1Moves);
-//   createAndAppendEl("button", "Special", "assassinSpec", fighter1Moves);
-//   createAndAppendEl("button", "Ultimate", "assassinUlt1", fighter1Moves);
-//   mainContainer.append(fighter1Div);
-// }
-// async function displayAssassinForP2(player2) {
-//   const fighter2Div = document.createElement("div");
-//   fighter2Div.classList.add("flexBox");
-//   fighter2Moves = document.createElement("div");
-//   fighter2Moves.classList.add("flex", "fighter2Moves");
-//   assassin2 = new Assassin();
-//   createAndAppendEl("h1", player2, "playername", fighter2Div);
-//   createAndAppendEl("p", assassin2.health, "assassin2HP", fighter2Div);
-//   createAndAppendEl("img", "./img/", "assasinImg", fighter2Div);
-//   fighter2Div.append(fighter2Moves);
-//   createAndAppendEl("button", "Dagger", "daggerBtn", fighter2Moves);
-//   createAndAppendEl("button", "Legsweep", "legsweepBtn", fighter2Moves);
-//   createAndAppendEl("button", "Special", "assassinSpec", fighter2Moves);
-//   createAndAppendEl("button", "Ultimate", "assassinUlt2", fighter2Moves);
-//   mainContainer.append(fighter2Div);
-// }
 function nextTurnP1() {
   const fighter1 = document.querySelector(".fighter1Moves");
   const fighter2 = document.querySelector(".fighter2Moves");
@@ -163,15 +96,5 @@ async function createAndAppendEl(type, content, addId, container) {
 
   return element;
 }
-console.log(createAndAppendEl);
 
-export {
-  // displayAssassinForP1,
-  // displayAssassinForP2,
-  // displayWarriorForP1,
-  // displayWarriorForP2,
-  displayPlayerOne,
-  displayPlayerTwo,
-  nextTurnP1,
-  nextTurnP2,
-};
+export { displayPlayerOne, displayPlayerTwo, nextTurnP1, nextTurnP2 };
